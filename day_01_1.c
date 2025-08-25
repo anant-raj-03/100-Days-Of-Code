873c79da110570f61bbdcc264a7d8b464e351741
Anant Raj <anant9304@gmail.com>
2025-08-25 19:15:55 +0530

    day_01_1.c

--- day_01_1.c
+++ day_01_1.c
@@ -2,15 +2,16 @@
 
 int main() {
      /*Write a program to input two numbers and display their sum.
-     1.num1 = 3 , num2 = 4
-     2.num1 = -1 , num2 = 20
+     1. a = 3 , b = 4
+     2. b = -1 , b = 20
      */
-    int num1, num2, sum;
+    int a, b, sum;
     printf("Enter the first integer: ");
-    scanf("%d", &num1);
+    scanf("%d", &a);
     printf("Enter the second integer: ");
-    scanf("%d", &num2);
-    sum = num1 + num2;
+    scanf("%d", &b);
+    sum = a + b;
     printf("sum = %d\n", sum);
     return 0;
+
 }
