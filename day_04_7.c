a57b9c3c5eb0c87251ec145d820ba62100e5f7ec
Anant Raj <anant9304@gmail.com>
2025-08-30 01:09:36 +0530

    Update day_04_7.c

--- day_04_7.c
+++ day_04_7.c
@@ -10,10 +10,11 @@ int main() {
     scanf("%d %d", &a, &b);
 
     // Swapping without using a third variable
-    a = a + b; // Step 1: sum of both
-    b = a - b; // Step 2: original a
-    a = a - b; // Step 3: original b
+    a = a + b; // Step 1:- sum of both
+    b = a - b; // Step 2:- original a
+    a = a - b; // Step 3:- original b
 
     printf("After swapping: a = %d, b = %d\n", a, b);
     return 0;
+
 }
